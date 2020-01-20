0ac655ecf9333781abb994633d1ec34a881f275d
Lakshithanaranwala <naranwala.lakshitha@gmail.com>
2020-01-20 23:43:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bus_ownwer01/map.java
@@ -0,0 +1,51 @@
+package com.example.bus_ownwer01;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class map extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap map;
+    public map() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_map, container, false);
+
+        return  view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment=(SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
